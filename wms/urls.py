a25394fe3d8acf4b4fd5b7365a0ef945f80cda9b
ilovezerg <trevor.b.nail@gmail.com>
2023-01-27 09:50:21 -0500

    url changes

--- wms/urls.py
+++ wms/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from wms_app.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
     path('wms/', include('wms_app.urls'))
 ]
