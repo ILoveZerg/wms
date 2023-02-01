91f195a3ad1fd928eb6751f0421b0e59cb0a1b45
ilovezerg <trevor.b.nail@gmail.com>
2023-02-01 15:53:15 -0500

    b

--- wms_app/views.py
+++ wms_app/views.py
@@ -285,7 +285,7 @@ def token(request):
     except OAuthError as e:
         res = None
     if res and res.ok:
-        logger.debug(res.get('accountID'))
+        logger.debug(res.accountID)
         request.session['user'] = res.json()
 
     return HttpResponseRedirect(reverse('items'))
