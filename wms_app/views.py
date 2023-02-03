7dee4d750bfe84731f6416c85af578c568b549a3
ilovezerg <trevor.b.nail@gmail.com>
2023-02-03 12:07:14 -0500

    b

--- wms_app/views.py
+++ wms_app/views.py
@@ -276,7 +276,7 @@ def token(request):
     request.session['token'] = token
     logger = logging.getLogger(__name__)
     try:
-        res = oauth.lightspeed.get('.json', token=OAuth2Token(token))
+        res = oauth.lightspeed.get('.json', token=token)
         logger.debug(res)
         logger.debug(res.text)
         logger.debug(res.json())
