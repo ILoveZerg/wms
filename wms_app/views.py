f7703f50e13e7a00ace87f935aee0484c7a897d8
ilovezerg <trevor.b.nail@gmail.com>
2023-02-06 14:26:26 -0500

    b

--- wms_app/views.py
+++ wms_app/views.py
@@ -273,7 +273,7 @@ def home(request):
 
 
 def login(request):
-    redirect_uri = 'http://nail.network/token'
+    redirect_uri = 'http://www.nail.network/token'
     return lightspeed.authorize_redirect(request, redirect_uri)
 
 
