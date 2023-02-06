ba4c32d87f09a8cd5fa82fc9b8e304e2d63b8560
ilovezerg <trevor.b.nail@gmail.com>
2023-02-06 11:27:11 -0500

    b

--- wms/settings.py
+++ wms/settings.py
@@ -27,12 +27,12 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.contrib.messages.middleware.MessageMiddleware'
 ]
 
 LOGGING = {
